212757844505d57d21f245bd6b4697b608511819
ahabanova <ahabanova95@gmail.com>
2023-04-30 14:46:20 +0200

    2. úkol

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const listElm = document.querySelector('.movie-list')
 fetch(`https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies`)
     .then(response => response.json())
     .then((data) => {
-
         data.forEach(film => {
             listElm.innerHTML += `
             <li class="movie-detail">
             <div class="movie-poster">
-                <img 
+                <img
                 src="${film.posterUrl}"
                 alt="${film.title}."
                 />
@@ -24,4 +23,45 @@ fetch(`https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies`)
             </li>
             `
         });
+    })
+
+
+//02
+const selectElm = document.querySelector('#select-genre')
+const formElm = document.querySelector('.filters')
+
+formElm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    fetch(`https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies?genre=${selectElm.value}`)
+        .then(response => response.json())
+        .then((data) => {
+            listElm.innerHTML = ''
+            data.forEach(film => {
+                listElm.innerHTML += `
+                            <li class="movie-detail">
+                            <div class="movie-poster">
+                                <img
+                                src="${film.posterUrl}"
+                                alt="${film.title}."
+                                />
+                            </div>
+                            <div class="movie-info">
+                                <h2 class="movie-title">${film.title}</h2>
+                                <div class="movie-year">Rok vydání: ${film.year}</div>
+                                <div class="movie-link">
+                                <a href="${film.url}" target="_blank">Odkaz na CSFD</a>
+                                </div>
+                            </div>
+                            </li>
+                            `        })
+        })
+})
+
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/genres')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(genre => {
+            selectElm.innerHTML += `<option value="${genre}">${genre[0].toUpperCase() + genre.slice(1)}</option>`
+        })
     })
